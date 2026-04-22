ba3472cfb0b8800acbf160690747fed8c407821b
Nutchakrit <nut_hops@hotmail.com>
2026-04-22 21:29:36 +0900

    Version 1.2.6 Update

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const APP_VERSION = "1.2.5";
+const APP_VERSION = "1.2.6";
 const CACHE_NAME = "nck-portal-v" + APP_VERSION;
--- yen/config.js
+++ yen/config.js
@@ -1,2 +1,2 @@
-const APP_VERSION = "1.2.5";
+const APP_VERSION = "1.2.6";
 const CACHE_NAME = "necomaid-calc-v" + APP_VERSION;
