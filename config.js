8cea46be73b8697d02ebe057af8b5f4beecad517
Nutchakrit Tuntivanitchphong <nut_hops@hotmail.com>
2026-04-28 10:31:13 +0900

    2.3 Update

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const APP_VERSION = "2.2.2";
+const APP_VERSION = "2.3";
 const CACHE_NAME = "nck-portal-v" + APP_VERSION;
--- mart/config.js
+++ mart/config.js
@@ -1,2 +1,2 @@
-const APP_VERSION = "2.2.2";
+const APP_VERSION = "2.3";
 const CACHE_NAME = "necomaid-calc-v" + APP_VERSION;
