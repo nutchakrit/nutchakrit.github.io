6bb472e506929bcaa8b22f19ca60ce7297a0c6a4
Nutchakrit Tuntivanitchphong <nut_hops@hotmail.com>
2026-05-07 21:55:43 +0700

    2.8 Update

--- baht/config.js
+++ baht/config.js
@@ -1,2 +1,2 @@
-const APP_VERSION = "2.7";
+const APP_VERSION = "2.8";
 const CACHE_NAME = "necomart-thb-v" + APP_VERSION;
--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const APP_VERSION = "2.7";
+const APP_VERSION = "2.8";
 const CACHE_NAME = "nck-portal-v" + APP_VERSION;
