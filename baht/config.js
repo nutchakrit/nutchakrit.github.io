8d065ec996b70720c4045a805958c836c2da6de6
Nutchakrit Tuntivanitchphong <nut_hops@hotmail.com>
2026-05-08 10:48:18 +0700

    3.0 Update

--- baht/config.js
+++ baht/config.js
@@ -1,2 +1,2 @@
-const APP_VERSION = "2.9";
+const APP_VERSION = "3.0";
 const CACHE_NAME = "necomart-thb-v" + APP_VERSION;
--- mart/config.js
+++ mart/config.js
@@ -1,2 +1,2 @@
-const APP_VERSION = "2.9";
+const APP_VERSION = "3.0";
 const CACHE_NAME = "necomaid-calc-v" + APP_VERSION;
